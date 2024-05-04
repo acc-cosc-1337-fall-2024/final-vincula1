1988e6f21da7ff97d748718e4a0c8c4b8fb67080
Laith Khireiwish <141301612+vincula1@users.noreply.github.com>
2024-05-04 11:54:01 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,12 @@
 //
+#include "die.h"
+#include <cstdlib>
+#include <ctime>
+
+Die::Die() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+}
+
+int Die::roll() const {
+    return std::rand() % sides + 1;
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,15 @@
 //
+#ifndef DIE_H
+#define DIE_H
+
+class Die {
+public:
+    Die();
+    int roll() const;
+
+private:
+    const int sides = 6;
+
+};
+
+#endif
