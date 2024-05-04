0a24b689d2fdea5799468d549268beb58e410336
Laith Khireiwish <141301612+vincula1@users.noreply.github.com>
2024-05-04 12:20:26 +0000

    Question 2

--- src/roll.cpp
+++ src/roll.cpp
@@ -1,1 +1,14 @@
 //
+#include "roll.h"
+
+
+Roll::Roll(Die& die1, Die& die2) : die1(die1), die2(die2), rolled_value(0) {}
+
+void Roll::roll_dice() {
+    rolled_value = die1.roll() + die2.roll();
+}
+
+
+int Roll::roll_value() const {
+    return rolled_value;
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -21,6 +22,30 @@ TEST_CASE("Rolling a dice between 10 times to see if it returns value 1-6", "[Di
             int result = die.roll();
             REQUIRE(result >= 1);
             REQUIRE(result <= 6);
+        }
+    }
+}
+
+
+TEST_CASE("Test case to test roll for value 2 and 112", "[Die]") {
+    Die die1, die2;
+    Roll roll(die1, die2);
+    roll.roll_dice();
+    int result = roll.roll_value();
+    REQUIRE(result >= 1);
+    REQUIRE(result <= 6);
+}
+
+TEST_CASE("Testing die rolls between 2 and 12 , 10 rolls", "[Roll]") {
+    Die die1, die2;
+    Roll roll(die1, die2);
+
+    SECTION("Verify dice roll values") {
+        for (int i = 0; i < 10; i++) {
+            roll.roll_dice();
+            int result = roll.roll_value();
+            REQUIRE(result >= 2);
+            REQUIRE(result <= 12);
         }
     }
 }
